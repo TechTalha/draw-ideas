7d81766d49b53b034ba3a0f87291ffcc73c0a0a5
Talha <101164152+TechTalha@users.noreply.github.com>
2023-12-01 00:17:44 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+// hello
+import { Tldraw } from '@tldraw/tldraw'
+import '@tldraw/tldraw/tldraw.css'
+
+export default function () {
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw />
+		</div>
+	)
+}
